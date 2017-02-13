0a6b19a561187e96f0ee2a8dfc3559a96282142b
cipriancus <cipriancus@gmail.com>
2017-02-13 09:59:45 +0200

    Commit check

--- AnonymousChordLibrary/AnonymousChord/p2p/protocols/chord/ChordNode.h
+++ AnonymousChordLibrary/AnonymousChord/p2p/protocols/chord/ChordNode.h
@@ -1,13 +1,3 @@
-/*
- *  ChordNode.h
- *  iPhone_p2p_engine
- *
- *  Created by Laurent Vanni & Nicolas Goles Domic, 2010
- *
- *	This class is the implementation of the "Chord" class,
- *	it inherits from the Chord abstract class and implements
- *	it's abstract methods.
- */
 
 #ifndef CHORDNODE_H
 #define CHORDNODE_H
